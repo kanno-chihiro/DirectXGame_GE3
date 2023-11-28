a184838ddbfd81b7c5d424a46c9fb27101c88996
菅野　地紘 <k022g0111@g.neec.ac.jp>
2023-11-28 15:33:39 +0900

    三章完成までこれでやってしまった

--- Input.cpp
+++ Input.cpp
@@ -1,23 +1,17 @@
 #include "Input.h"
 #include <cassert>
 
-#pragma comment(lib,"dinput8.lib")
-#pragma comment(lib,"dxguid.lib")
-
-#define DIRECTINPUT_VERSION     0x0800   // DirectInputのバージョン指定
-#include <dinput.h>
-
-#include <wrl.h>
 
 using namespace Microsoft::WRL;
 
-void Input::Initialize(HINSTANCE hInstance, HWND hwnd)
+void Input::Initialize(WinApp* winApp)
 {
+    winApp_ = winApp;
     HRESULT result;
     // DirectInputの初期化
     ComPtr<IDirectInput8> directInput;
     result = DirectInput8Create(
-        hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&directInput, nullptr);
+        winApp_->GetHinstance(), DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&directInput, nullptr);
     assert(SUCCEEDED(result));
 
     
@@ -27,7 +21,7 @@ void Input::Initialize(HINSTANCE hInstance, HWND hwnd)
     assert(SUCCEEDED(result));
     // 排他制御レベルのセット
     result = keyboard->SetCooperativeLevel(
-        hwnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY);
+        winApp_->GetHwnd(), DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY);
     assert(SUCCEEDED(result));
 }
 
--- Input.h
+++ Input.h
@@ -1,17 +1,23 @@
 #pragma once
 
 #include <windows.h>
+#include <wrl.h>
 
 #define DIRECTINPUT_VERSION  0x0800//DirectInputのバージョン指定
 #include <dinput.h>
 
-#include <wrl.h>
+#pragma comment(lib,"dinput8.lib")
+#pragma comment(lib,"dxguid.lib")
+
+
+#include "WinApp.h"
+//using namespace Microsoft::WRL;
 
 class Input
 {
 public://メンバ関数
 	//初期化
-	void Initialize(HINSTANCE hinstance,HWND hwnd);
+	void Initialize(WinApp*winApp);
 	//更新
 	void Update();
 
@@ -20,15 +26,18 @@ public://メンバ関数
 
 	bool TriggerKey(BYTE keyNumber);
 
-	template <class T>using ComPtr = Microsoft::WRL::ComPtr<T>;
+	//template <class T>using ComPtr = Microsoft::WRL::ComPtr<T>;
 
 private:
+	WinApp* winApp_ = nullptr;
+
 	// キーボードデバイスの生成
 	Microsoft::WRL::ComPtr<IDirectInputDevice8> keyboard;
 
 	BYTE key[256] = {};
 
 	BYTE keyPre[256] = {};
+	
 
 };
 
